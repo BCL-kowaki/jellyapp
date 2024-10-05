15fbf18c70e4e29d2cde0e782b9e0536d3fcf566
BCL-kowaki <info@be-creative-labo.com>
2024-10-05 21:49:27 +0900

    ロゴ画像修正

--- jelly/app/(root)/(home)/page.tsx
+++ jelly/app/(root)/(home)/page.tsx
@@ -12,8 +12,8 @@ const Home = () => {
     <section className='flex flex-col gap-10 px-10 pt-7 text-white menuContent'>
       <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
         <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
-          <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-noemal'>
-            Upcoming Game at: 12:30 PM
+          <h2 className='max-w-[270px] rounded py-2 text-center text-base font-noemal'>
+            {/* Upcoming Game at: 12:30 PM */}
           </h2>
           <div className='flex flex-col gap-2'>
             <h2 className='text-4xl font-extrabold lg:text-7xl'>{time}</h2>
